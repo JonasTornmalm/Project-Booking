35f3eecfae4d56607d01ca3c2d87152fb4a4c3d6
AndreeNoA <55133543+AndreeNoA@users.noreply.github.com>
2020-04-14 10:15:57 +0200

    WEB8-38 Added context for connection to DB

---
+++ Project Booking/Model/ConnectionContextDb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Project_Booking
+{
+    public class ConnectionContextDb : DbContext
+    {
+        public ConnectionContextDb(DbContextOptions<ConnectionContextDb> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
