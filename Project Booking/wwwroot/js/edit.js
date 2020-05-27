e7ae91b757fe6d15dd3f736feea215e8a60a33e1
AndreeNoA <55133543+AndreeNoA@users.noreply.github.com>
2020-05-27 14:07:58 +0200

    WEB8-78 | Made edit page show available rooms, and only allow booking if rooms and dates are correct

--- Project Booking/wwwroot/js/edit.js
+++ Project Booking/wwwroot/js/edit.js
@@ -4,12 +4,14 @@ $(document).ready(function () {
     var firstday = new Date(document.getElementById("checkin").value);
     var timeDiff = (lastday.getTime() - firstday.getTime());
     var daysDiff = timeDiff / (1000 * 3600 * 24);
+    document.getElementById("checkDates").style.display = 'none';
 
     if (isNaN(daysDiff)) {
-        document.getElementById("days").innerHTML = "Pick a check-in and a check-out date"
+        document.getElementById("days").innerHTML = "Please check your dates"
     }
     else {
         document.getElementById("days").innerHTML = daysDiff;
     }
     updateDue();
 });
+    
