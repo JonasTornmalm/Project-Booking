93e761ffc023563d41d1145a6ac6517c93706f57
AndreeNoA <55133543+AndreeNoA@users.noreply.github.com>
2020-05-28 15:05:29 +0200

    WEB8-97 | Added Chuck Norris joke generator

---
+++ Project Booking/Model/Jokes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_Booking.Model
+{
+    public class Jokes
+    {
+        public string value { get; set; }
+        public string icon_url { get; set; }
+    }
+}
