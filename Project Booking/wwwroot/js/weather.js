a069ebf79acc311794f1a566a9575d85990fd113
Johan Andersson <j0han_91@hotmail.com>
2020-06-01 14:03:19 +0200

    changed the layout for weather API

--- Project Booking/wwwroot/js/weather.js
+++ Project Booking/wwwroot/js/weather.js
@@ -16,13 +16,14 @@
 }
 
 function show(data) {
-    return '<div>'+
-                '<div class="col-md-10"  >'+
-                    "<img style='float:left;width:150x;height:130px; margin-top:5px; margin-left:45%; text-align:right;' src='http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png' />" +
-                    '<h5 style="text-align: left"><br><strong>Temperature</strong>: ' + data.main.temp + '&deg;C</h5>' +
-                    '<h5 style="text-align: left"><strong>Weather</strong>: ' + data.weather[0].main + '</h5>' +
-                    '<h5 style="text-align: left"><strong>Wind Speed</strong>: ' + data.wind.speed + 'm/s</h5>' +
-                    '<h5 style="text-align: left"><strong>Wind Direction</strong>: ' + data.wind.deg + '&deg;</h5>' +
-                '</div>'+
-           '</div>';
+    return '<div >' +
+        '<div style="text-align:center">' +
+        "<img style='width:150x;height:130px; margin-top:5px;' src='http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png' />" +
+        '<h5><br><strong>Temperature</strong>: ' + data.main.temp + '&deg;C</h5>' +
+        '<h5><strong>Weather</strong>: ' + data.weather[0].main + '</h5>' +
+        '<h5><strong>Wind Speed</strong>: ' + data.wind.speed + 'm/s</h5>' +
+        '<h5><strong>Wind Direction</strong>: ' + data.wind.deg + '&deg;</h5>' +
+        '</div>' +
+        '</div>';
 }
+
